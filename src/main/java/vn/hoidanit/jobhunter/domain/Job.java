cb43aebbe3932a702e6dcf2987ecf26712ae904d
= <=>
2024-07-30 09:42:33 +0700

    Model Job

--- src/main/java/vn/hoidanit/jobhunter/domain/Job.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Job.java
@@ -11,49 +11,55 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
+import vn.hoidanit.jobhunter.util.constant.LevelEnum;
 
-@Table(name = "companies")
 @Entity
+@Table(name = "jobs")
 @Getter
 @Setter
-public class Company {
-
+public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "name không được để trống")
     private String name;
+    private String location;
+    private double salary;
+    private int quantity;
+    private LevelEnum level;
 
     @Column(columnDefinition = "MEDIUMTEXT")
     private String description;
 
-    private String address;
-
-    private String logo;
-
-    // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
+    private Instant startDate;
+    private Instant endDate;
+    private boolean isActive;
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
 
-    // LAZY và EAGER
-    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
-    @JsonIgnore
-    List<User> users;
 
-    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+    // 1 company nhận nhiều job, nhưng 1 job chỉ của 1 công ty
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    // 1 job có nhiều skill và 1 Skill có nhiều job quan ManyToMany bỏ bên skill or job gi cũng dcdc
+    @ManyToMany(fetch = FetchType.LAZY)
     @JsonIgnore
-    List<Job> jobs;
+    @JoinTable(name = "job_skill", joinColumns = @JoinColumn(name = "job_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
+    private List<Skill> skills; // lấy mappby ở skills
 
     @PrePersist
     public void handleBeforeCreate() {
